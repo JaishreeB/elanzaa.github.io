2469b3b54a76a3c00912cf1afe69abdae237e60f
JaishreeB <jaishreebagya@gmail.com>
2024-02-13 00:25:34 +0530

    first commit

---
+++ js/forms.js
@@ -0,0 +1,36 @@
+function inovaForm() {
+  var formUrl = "https://docs.google.com/forms/d/e/1FAIpQLSfzAq_4E0V5jfmQNyL56W1vwhNATATih6BpC3AbmdXghsgxpg/viewform?usp=sf_link";
+  window.open(formUrl);
+}
+function codeForm() {
+  var formUrl = "https://docs.google.com/forms/d/e/1FAIpQLScnUy73qmDZvkQI4XS0LHehM0OOQtV3nNGamGTipJ9Au8uikw/viewform?usp=sf_link";
+  window.open(formUrl);
+}
+function uiForm() {
+  var formUrl = "https://docs.google.com/forms/d/e/1FAIpQLSdPlZy3QHWakBVBdoKIcyD5GielNjqGktkR9vI7tbyKcgUf8w/viewform?usp=sf_link";
+  window.open(formUrl);
+}
+function pitchForm() {
+  var formUrl = "https://docs.google.com/";
+  window.open(formUrl);
+}
+function frontForm() {
+  var formUrl = "https://docs.google.com/forms/d/e/1FAIpQLSdOrU6tBudlLVQYf4jwaALAFuAl8xwckcFv5siP933jRaw3bw/viewform?usp=sf_link";
+  window.open(formUrl);
+}
+function nontechForm() {
+  var formUrl = "https://forms.gle/NMzSpUv2AXkeydgR8";
+  window.open(formUrl);
+}
+function techForm() {
+  var formUrl = "https://forms.gle/NMzSpUv2AXkeydgR8";
+  window.open(formUrl);
+}
+function workForm() {
+  var formUrl = "https://docs.google.com/forms/d/e/1FAIpQLSd8-Ha3FW9xZBnhlCf7j-QrCTRw6mY79C6tDTjVNSdqlPPFCg/viewform";
+  window.open(formUrl);
+}
+// function hackForm() {
+//   var formUrl = "https://docs.google.com/forms/d/e/1FAIpQLSfKrNzTO2uFWZKWcos4gbi3J7dr5WJgjRUCA5LvzBjdMO1WJg/viewform";
+//   window.open(formUrl);
+// }
